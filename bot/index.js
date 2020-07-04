2b4782605441897214360b74edeb974e4f3c7e15
Vincent James <viayaqtx>
2020-07-04 16:08:39 +0800

    Procfile, webhook

--- bot/index.js
+++ bot/index.js
@@ -6,13 +6,13 @@ const start = () => {
     const bot = new Telegraf(process.env.BOT_TOKEN)
     bot.start((context) => startMessage(context))
     bot.on('text', (context) => setReminder(context))
-    
+
     if (process.env.APP_ENV === 'local') {
         bot.telegram.deleteWebhook()
         bot.startPolling()
     } else {
         bot.telegram.setWebhook(`${process.env.WEBHOOK}/${process.env.SECRET}`)
-        bot.startWebhook(`/${process.env.SECRET}`)
+        bot.startWebhook(`/${process.env.SECRET}`, null, process.env.PORT)
     }
 }
 
