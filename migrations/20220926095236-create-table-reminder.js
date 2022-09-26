1769ef625421ace7e68bb0401c785ab9fef1b59a
Vincent James <viayaqtx>
2022-09-26 20:01:29 +0800

    migration

---
+++ migrations/20220926095236-create-table-reminder.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('reminder', {
+    id: { type: 'bigint', primaryKey: true, autoIncrement: true },
+    chat_id: { type: 'string', length: 255 },
+    date: 'date',
+    time: 'time',
+    activity: 'text',
+    frequency: { type: 'string', length: 255 },
+    created_at: { type: 'timestamp', default: 'CURRENT_TIMESTAMP' },
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('reminder');
+};
+
+exports._meta = {
+  version: 1,
+};
