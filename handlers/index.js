7bcc6a8d4c9ef429e2335828ef1b4f298138c81a
Vincent James <viayaqtx>
2022-09-26 17:20:00 +0800

    message update

--- handlers/index.js
+++ handlers/index.js
@@ -1,12 +1,10 @@
 const startMessage = (name) => {
     const message = `
-Hi ${name}! I'm Kafra.
+Hi ${name}! I'm Kafra .
 I can help you remember stuff.
 \r
 Give it a try by typing your first reminder:
 \`Get a snack in 5mins\`
-\r
-Because I am only using a free service, reminders are limited to 30mins only. Sorry about that.
 `
     return message
 }
